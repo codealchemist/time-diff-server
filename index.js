dbca39d0322466e8638a60f5925197ed7db33ed9
Alberto Miranda <amiranda@elementum.com>
2017-05-01 17:53:16 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const WebSocket = require('ws')
+const port = 8080
+const wss = new WebSocket.Server({
+  perMessageDeflate: false,
+  port
+}, onListening)
+const events = {
+  time: onTime
+}
+
+function onListening () {
+  log(`LISTENING ON PORT ${port}`)
+}
+
+wss.on('connection', (ws) => {
+  const host = ws.upgradeReq.headers.host
+  log('client connected:', host)
+
+  ws.on('message', (data) => {
+    log(`got message from ${host}`, data)
+    const {type, value} = JSON.parse(data)
+    if (!events[type]) return
+    events[type](ws, value)
+  })
+})
+
+function send (ws, message) {
+  const data = JSON.stringify(message)
+  ws.send(data)
+}
+
+function onTime (ws) {
+  const time = (new Date()).getTime()
+  send(ws, {type: 'time', value: time})
+}
+
+function log () {
+  console.log('[ TIME-DIFF-SERVER ]-->', ...arguments)
+}
